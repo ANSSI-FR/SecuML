ANSSI-FR/SecuML

commit 4cd4f8441169320b6926e3a93855816502612621

JS code refactoring
- refactoring of diadem monitoring. Does not require classifier_conf
  anymore. All the necessay information is stored in the DB in the
  diadem_exp table.
- createPanel takes now the id of the body as an optional argument.
- rename the perf. monitoring div. with shorter names.

secuml/exp/diadem/__init__.py
@@ -64,8 +64,10 @@ class InvalidModelExperimentKind(SecuMLexpException):
 
 
 def add_diadem_exp_to_db(session, exp_id, fold_id, kind, alerts_conf=None,
-                         classifier_conf=None, classifier_tup=None):
+                         classifier_conf=None):
     if classifier_conf is not None:
+        multiclass = classifier_conf.multiclass
+        proba = classifier_conf.is_probabilist()
         if kind in ['train', 'cv']:
             perf_monitoring = True
             alerts = False
@@ -77,16 +79,13 @@ def add_diadem_exp_to_db(session, exp_id, fold_id, kind, alerts_conf=None,
             predictions_interp = False
         else:
             predictions_interp = classifier_conf.interpretable_predictions()
-    elif classifier_tup is not None:
-        model_interp, predictions_interp = classifier_tup
-        alerts = False
-        perf_monitoring = True  # used only for AlreadyTrained models.
+        exp = DiademExpAlchemy(exp_id=exp_id, fold_id=fold_id, type=kind,
+                               perf_monitoring=perf_monitoring, alerts=alerts,
+                               model_interpretation=model_interp,
+                               predictions_interpretation=predictions_interp,
+                               multiclass=multiclass, proba=proba)
     else:
-        assert(False)
-    exp = DiademExpAlchemy(exp_id=exp_id, fold_id=fold_id, type=kind,
-                           perf_monitoring=perf_monitoring, alerts=alerts,
-                           model_interpretation=model_interp,
-                           predictions_interpretation=predictions_interp)
+        exp = DiademExpAlchemy(exp_id=exp_id, fold_id=fold_id, type=kind)
     session.add(exp)
     session.flush()
 
@@ -181,7 +180,7 @@ class DiademExp(Experiment):
         else:
             raise InvalidModelExperimentKind(model_exp.kind)
 
-    def _create_test_exp(self, fold_id=None):
+    def _create_test_exp(self, classifier, fold_id=None):
         diadem_id = self.exp_conf.exp_id
         exp_name = 'DIADEM_%i_Test' % diadem_id
         if fold_id is not None:
@@ -202,7 +201,7 @@ class DiademExp(Experiment):
                                  self.exp_conf.core_conf.classifier_conf,
                                  name=exp_name, parent=diadem_id,
                                  fold_id=fold_id, kind='test')
-        return TestExp(test_exp_conf,
+        return TestExp(test_exp_conf, classifier=classifier,
                        alerts_conf=self._get_alerts_conf(fold_id),
                        session=self.session)
 
@@ -252,8 +251,8 @@ class DiademExp(Experiment):
             return train_exp.classifier
 
     def _test(self, classifier, datasets, fold_id):
-        test_exp = self._create_test_exp(fold_id=fold_id)
-        test_exp.run(classifier, datasets.test_instances)
+        test_exp = self._create_test_exp(classifier, fold_id=fold_id)
+        test_exp.run(datasets.test_instances)
         self._set_train_test_exp('test', test_exp, fold_id)
         return test_exp.predictions
 

secuml/exp/diadem/alerts_clustering.py
@@ -28,5 +28,4 @@ class AlertsClusteringExp(ClusteringExperiment):
         ClusteringExperiment.add_to_db(self)
         # add the AlertsClustering exp. to the DB.
         from secuml.exp.diadem import add_diadem_exp_to_db
-        add_diadem_exp_to_db(self.session, self.exp_id, None, 'alerts',
-                             classifier_tup=(False, False))
+        add_diadem_exp_to_db(self.session, self.exp_id, None, 'alerts')

secuml/exp/diadem/train_test.py
@@ -86,19 +86,19 @@ class TrainExp(Experiment):
 
 class TestExp(Experiment):
 
-    def __init__(self, exp_conf, alerts_conf=None, create=True, session=None):
-        Experiment.__init__(self, exp_conf, create=create, session=session)
+    def __init__(self, exp_conf, classifier=None, alerts_conf=None,
+                 create=True, session=None):
+        self.classifier = classifier
         self.kind = exp_conf.kind
         self.alerts_conf = alerts_conf
         self.test_instances = None
-        self.classifier = None
         self.predictions = None
         self.monitoring = None
+        Experiment.__init__(self, exp_conf, create=create, session=session)
 
-    def run(self, classifier, test_instances):
+    def run(self, test_instances):
         Experiment.run(self)
         self.test_instances = self.get_instances(test_instances)
-        self.classifier = classifier
         self._test()
         self._export()
 
@@ -108,7 +108,7 @@ class TestExp(Experiment):
         add_diadem_exp_to_db(self.session, self.exp_conf.exp_id,
                              self.exp_conf.fold_id, self.kind,
                              alerts_conf=self.alerts_conf,
-                             classifier_conf=self.exp_conf.core_conf)
+                             classifier_conf=self.classifier.conf)
 
     def _test(self):
         if self.test_instances.has_ground_truth():

secuml/exp/tools/db_tables.py
@@ -105,6 +105,8 @@ class DiademExpAlchemy(Base):
     perf_monitoring = Column(Boolean, nullable=True)
     model_interpretation = Column(Boolean, nullable=True)
     predictions_interpretation = Column(Boolean, nullable=True)
+    multiclass = Column(Boolean, nullable=True)
+    proba = Column(Boolean, nullable=True)
 
     exp = relationship('ExpAlchemy', back_populates='diadem_exp',
                        uselist=False)

secuml/web/static/js/active_learning/current_annotations.js
@@ -106,7 +106,8 @@ function updateInstanceNavbar() {
 }
 
 function displayButton(buttons_group, l, type) {
-    var label_group = createDivWithClass(null, 'btn-group', parent_div=buttons_group);
+    var label_group = createDivWithClass(null, 'btn-group',
+                                         parent_div=buttons_group);
     var label_button = document.createElement('button');
     label_button.setAttribute('class', 'btn btn-lg btn-' + type);
     label_button.setAttribute('type', 'button');
@@ -124,14 +125,14 @@ function displayButtons() {
                                            parent_div=main);
     displayButton(buttons_group, 'malicious', 'danger');
     displayButton(buttons_group, 'benign', 'success');
-    var nav_bars = createDivWithClass('nav_bars', 'col-md-12', parent_div=main);
+    var nav_bars = createDivWithClass('nav_bars', 'col-md-12',
+                                      parent_div=main);
 }
 
 function displayNavbars(type) {
     var nav_bars = cleanDiv('nav_bars');
     var panel_body = createPanel('panel-' + type, 'col-md-6',
-            'Annotated Instances',
-            nav_bars);
+                                 'Annotated Instances', nav_bars);
     displayFamiliesBar(panel_body, type);
     displayInstancesBar(panel_body, type);
     var query = buildQuery('getFamiliesInstances',

secuml/web/static/js/active_learning/edit_families.js
@@ -93,7 +93,8 @@ function displayRename(modif_div) {
   }
 
   cleanDiv(modif_div.id);
-  var panel = createPanel('panel-primary', 'row', 'Rename a Family', modif_div);
+  var panel = createPanel('panel-primary', 'row', 'Rename a Family',
+                          modif_div);
   displayFamiliesSelectors(panel);
 
   // Label
@@ -282,10 +283,10 @@ function displayDivisions() {
   var stats_panel = createPanel('panel-primary', 'col-md-12',
                                 'Number of Instances per Family', stats_row);
   var stats_panel_m = createPanel('panel-danger', 'col-md-6',
-                                  'Malicious Families', stats_panel);
-  stats_panel_m.setAttribute('id', 'stats_panel_malicious');
+                                  'Malicious Families', stats_panel,
+                                  'stats_panel_malicious');
   var stats_panel_b = createPanel('panel-success', 'col-md-6',
-                                  'Benign Families', stats_panel);
-  stats_panel_b.setAttribute('id', 'stats_panel_benign');
+                                  'Benign Families', stats_panel,
+                                  'stats_panel_benign');
   updateFamiliesStats();
 }

secuml/web/static/js/active_learning/ilab_annotations.js
@@ -229,9 +229,8 @@ function displayButtons() {
 
 function displayNavbars(type, annotations_type, clustering_exp) {
     var nav_bars = cleanDiv('nav_bars');
-    var panel_body = createPanel('panel-' + type, 'row',
-            'Annotation Queries',
-            nav_bars);
+    var panel_body = createPanel('panel-' + type, 'row', 'Annotation Queries',
+                                 nav_bars);
     var col = createDivWithClass(null, 'col-md-10', panel_body);
     if (annotations_type == 'families') {
         displayFamiliesBar(col, type);

secuml/web/static/js/active_learning/individual_annotations.js
@@ -67,8 +67,7 @@ function displayDivisions(conf) {
 
     // Navigation bar
     var row = createDivWithClass(null,  'row', parent_div = main);
-    var panel_body = createPanel('panel-primary', 'col-md-12',
-            null, row);
+    var panel_body = createPanel('panel-primary', 'col-md-12', null, row);
     var annotation_query_label = document.createElement('label');
     annotation_query_label.setAttribute('class', 'col-lg-2 control-label');
     annotation_query_label.appendChild(document.createTextNode('Annotation Query'));

commit 54cba7b71d0e76b5ae9246d471a77eda434c9756

Bug fix for ILAB with a validation dataset.
Bug introduced in 4cd4f8441169320b6926e3a93855816502612621.

secuml/exp/diadem/__init__.py
@@ -205,7 +205,7 @@ class DiademExp(Experiment):
                        alerts_conf=self._get_alerts_conf(fold_id),
                        session=self.session)
 
-    def _create_validation_exp(self, fold_id=None):
+    def _create_validation_exp(self, classifier, fold_id=None):
         assert(self.validation_conf.method == 'dataset')
         diadem_id = self.exp_conf.exp_id
         exp_name = 'DIADEM_%i_Validation' % diadem_id
@@ -223,7 +223,7 @@ class DiademExp(Experiment):
                                  self.exp_conf.core_conf.classifier_conf,
                                  name=exp_name, parent=diadem_id,
                                  kind='validation')
-        return TestExp(test_exp_conf,
+        return TestExp(test_exp_conf, classifier=classifier,
                        alerts_conf=self._get_alerts_conf(fold_id),
                        session=self.session)
 
@@ -257,8 +257,9 @@ class DiademExp(Experiment):
         return test_exp.predictions
 
     def _validate(self, classifier, datasets, fold_id):
-        validation_exp = self._create_validation_exp(fold_id=fold_id)
-        validation_exp.run(classifier, None)
+        validation_exp = self._create_validation_exp(classifier,
+                                                     fold_id=fold_id)
+        validation_exp.run(None)
         self._set_train_test_exp('validation', validation_exp, fold_id)
 
     def _run_cv(self, cv_datasets, cv_monitoring):