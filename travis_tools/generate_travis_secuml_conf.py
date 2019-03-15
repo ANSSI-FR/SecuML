4aae65acd0924b6d572aaef2b7356912f910b8f3
Anael Beaugnon <anael.beaugnon@ssi.gouv.fr>
2019-03-15 16:31:54 +0100

    Update copyright years.

--- travis_tools/generate_travis_secuml_conf.py
+++ travis_tools/generate_travis_secuml_conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # SecuML
-# Copyright (C) 2016-2018  ANSSI
+# Copyright (C) 2016-2019  ANSSI
 #
 # SecuML is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
